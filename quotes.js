c48eecd52ab95fa6c58e6a0b61fccc8b5990319d
facudelucia <facundodeluciajudo@gmail.com>
2020-09-13 09:35:10 -0300

    make it better

---
+++ quotes.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        "quote": "Me siento un jugador de ajedrez",
+        "author": "Alberto Fernández"
+    },
+    {
+        "quote": "Si abrimos la economia vamos a terminar como suecia",
+        "author": "Alberto Fernández"
+    },
+    {
+        "quote": "Alberto manejó la cuarentena mejor que Merkel",
+        "author": "Juan Grabois"
+    }
+]

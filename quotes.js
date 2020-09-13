aabc2f2c1409d42d69bc1fed04d1b712a99bf4f1
facudelucia <facundodeluciajudo@gmail.com>
2020-09-13 16:08:55 -0300

    frases agregadas

--- quotes.js
+++ quotes.js
@@ -10,5 +10,153 @@ module.exports = [
     {
         "quote": "Alberto manejó la cuarentena mejor que Merkel",
         "author": "Juan Grabois"
-    }
+    },
+    {
+        "quote": "Querían salir a correr y a pasear, salgan; ahí están las consecuencias",
+        "author": "Alberto Fernández"
+    },
+    {
+        "quote": "Este semestre los jubilados le están ganando a la inflacion",
+        "author": "Santiago Cafiero"
+    },
+    {
+        "quote": "Cualquier resfrío en este invierno es coronavirus hasta que se demuestre lo contrario",
+        "author": "Carla Vizzoti"
+    },
+    {
+        "quote": "Me equivoqué con Vicentin, pensé que la gente iba a salir a festejar",
+        "author": "Alberto Fernández"
+    },
+    {
+        "quote": "La emisión monetaria no es la causa de la inflación",
+        "author": "Cecilia Todesca"
+    },
+    {
+        "quote": "Francamente, no creo en los planes económicos",
+        "author": "Alberto Fernández"
+    },
+    {
+        "quote": "Creí que iban a bajar los números por la cuarentena anterior",
+        "author": "Ginés González García"
+    },
+    {
+        "quote": "No hay cuarentena",
+        "author": "Alberto Fernández"
+    },
+    {
+        "quote": "Yo creí que iba a llegar mucho mas tarde",
+        "author": "Ginés González García"
+    },
+    {
+        "quote": "No tenemos ninguna posibilidad de que exista el virus en el país",
+        "author": "Ginés González García"
+    },
+    {
+        "quote": "Al haber más exámenes es probable que haya más casos",
+        "author": "Ginés González García"
+    },
+    {
+        "quote": "Los empresarios no quieren a Moyano porque cuida a los suyos",
+        "author": "Alberto Fernández"
+    },
+    {
+        "quote": "Hugo Moyano es un dirigente sindical ejemplar",
+        "author": "Alberto Fernández"
+    },
+    {
+        "quote": "Cristina tiene una enorme distorsión sobre la realidad",
+        "author": "Alberto Fernández"
+    },
+    {
+        "quote": "Donar el sueldo es un acto de populismo que no resuelve nada",
+        "author": "Alberto Fernández"
+    },
+    {
+        "quote": "Hasta aquí vendieron más de 250 millones de dolares de reserva y el dólar supero holgadamente los 21 pesos. Y ahora? Llamen a los bomberos!!!",
+        "author": "Alberto Fernández"
+    },
+    {
+        "quote": "Nena, no es algo que me inquiete lo que vos creas. Mejor aprende a cocinar. Tal vez así logres hacer algo bien. Pensar no es tu fuerte. Está visto",
+        "author": "Alberto Fernández"
+    },
+    {
+        "quote": "Si bajamos los impuestos no nos queda un peso para nada",
+        "author": "Alberto Fernández"
+    },
+    {
+        "quote": "Coronavirus, cara de coronavirus eh",
+        "author": "Axel Kicillof"
+    },
+    {
+        "quote": "El segundo mandato de Cristina es lo peor que vi",
+        "author": "Alberto Fernández"
+    },
+    {
+        "quote": "Si sos un pibe de 18 años y tenés tres opciones: agarrar un chumbo, salir a robar y ganarte una buena guita por día e irte a tu casa tranca palanca con dos celus; otra opción es que te den una bolsa de falopa y vos te ocupás de venderla, otra opción muy rentable; la tercera opción es cargar un carro con cartones todo el día para capaz ganar unos mangos",
+        "author": "Ofelia Fernández"
+    },
+    {
+        "quote": "Si no hay un mango partido al medio, lo qué 'haiga' para el que más necesita",
+        "author": "Axel Kicillof"
+    },
+    {
+        "quote": "Hay gente que se dedica a vender droga porque se quedó sin laburo",
+        "author": "Axel Kicillof"
+    },
+    {
+        "quote": "Si me hubiese tocado salir a juntar cartones, yo voy de caño",
+        "author": "Axel Kicillof"
+    },
+    {
+        "quote": "Si algún muchacho quiere vivir de lo ajeno, vive de lo ajeno, pero con códigos",
+        "author": "Guillermo Moreno"
+    },
+    {
+        "quote": "El virus cruza la general paz cuando quiere",
+        "author": "Axel Kicillof"
+    },
+    {
+        "quote": "No es libertad, es otorgar prision domiciliaria",
+        "author": "Ofelia Fernández"
+    },
+    {
+        "quote": "Me hice una carta astral: estoy predestinado a construir desde las cenizas",
+        "author": "Alberto Fernández"
+    },
+    {
+        "quote": "Les pibis",
+        "author": "Lucas Grimson"
+    },
+    {
+        "quote": "Cuando se mandan una cagada, que me venden falopa, yo los tengo que cubrir. Cuando me estaban vendiendo falopa con las ambulancias",
+        "author": "Mario Ishii"
+    },
+    {
+        "quote": "La Argentina de los vivos se terminó",
+        "author": "Alberto Fernández"
+    },
+    {
+        "quote": "Es la provincia más grande de Argentina si no contamos la Antártida",
+        "author": "Axel Kicillof"
+    },
+    {
+        "quote": "Si o si vamos a ser Venezuela, si o si vamos a hacer populismo, así que empecemos ahora muchachos y terminemos con esta agónica espera",
+        "author": "Dady Brieva"
+    },
+    {
+        "quote": "Averigué sobre mi salario en caso de asumir, que me parece una cosa descomunal, que no quiero cobrar",
+        "author": "Ofelia Fernández"
+    },
+    {
+        "quote": "Alberto Fernández te milité como nunca milité a nadie, durante los años de Macri, y cuando Cristina decidió que vos fueras el candidato del frente de todos te milité más de 100mil km. para que ahora me claves el visto en el teléfono, para que no me atiendas las llamadas y te hagas el pelotudo Alberto",
+        "author": "Ezequiel Guazzora"
+    },
+    {
+        "quote": "Estaban los oligarcas especulando con el precio del dólar porque ahí estaban los silobolsas. Que lindo sería que alguien, con una navajita nomás, y se los abra",
+        "author": "Ezequiel Guazzora"
+    },
+    {
+        "quote": "Ocupar no es usurpar",
+        "author": "Juan Grabois"
+    },
 ]
